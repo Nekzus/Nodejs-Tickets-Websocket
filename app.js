9a4fb98f5724af6b4952ff83797a46d29b28ef31
Nekzus <85507589+Nekzus@users.noreply.github.com>
2022-08-17 00:46:41 -0300

    Emision-Broadcasting servidor

---
+++ app.js
@@ -0,0 +1,8 @@
+import dotenv from "dotenv/config";
+import { Server } from "./models/server.js";
+
+const server = new Server();
+
+server.listen();
+
+process.removeAllListeners("warning");
---
+++ models/ticket-control.js
@@ -0,0 +1,75 @@
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import data from "../db/data.json" assert { type: "json" };
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+class Ticket {
+  constructor(numero, escritorio) {
+    this.numero = numero;
+    this.escritorio = escritorio;
+  }
+}
+
+class TicketControl {
+  constructor() {
+    this.ultimo = 0;
+    this.hoy = new Date().getDate();
+    this.tickets = [];
+    this.ultimos4 = [];
+
+    this.init();
+  }
+
+  get toJson() {
+    return {
+      ultimo: this.ultimo,
+      hoy: this.hoy,
+      tickets: this.tickets,
+      ultimos4: this.ultimos4,
+    };
+  }
+
+  init() {
+    const { hoy, tickets, ultimo, ultimos4 } = data;
+    if (hoy === this.hoy) {
+      this.tickets = tickets;
+      this.ultimo = ultimo;
+      this.ultimos4 = ultimos4;
+    } else {
+      // Es otro dia
+      this.guardarDB();
+    }
+  }
+
+  guardarDB() {
+    const dbPath = path.join(__dirname, "../db/data.json");
+    fs.writeFileSync(dbPath, JSON.stringify(this.toJson));
+  }
+
+  siguiente() {
+    this.ultimo += 1;
+    const ticket = new Ticket(this.ultimo, null);
+    this.tickets.push(ticket);
+
+    this.guardarDB();
+    return "Ticket " + ticket.numero;
+  }
+
+  atenderTicket(escritorio) {
+    // No tenemos tickets
+    if (this.tickets.length === 0) {
+      return null;
+    }
+    const ticket = this.tickets.shift();
+    ticket.escritorio = escritorio;
+    this.ultimos4.unshift(ticket);
+    if (this.ultimos4.length > 4) {
+      this.ultimos4.splice(-1, 1);
+    }
+    this.guardarDB();
+    return ticket;
+  }
+}
+
+export { TicketControl };
